ca1d2018b1a677ae5dacc7cfb774a3bbc9296154
Arkadiy Kukarkin <arkadiy@mindhole.org>
2015-10-21 12:32:46 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var Knex = require('knex');
+var config = require('./knexfile');
+var pg = require('pg');
+var Redis = require('redis');
+var env = require('require-env');
+
+pg.types.setTypeParser(20, 'text', parseInt);
+const node_env = process.env.NODE_ENV || 'development';
+const knex = Knex(config[node_env]);
+
+const REDIS_PROVIDER = env.require('REDIS_PROVIDER');
+const REDIS_URL = env.require(REDIS_PROVIDER);
+const redis = Redis.createClient(REDIS_URL);
