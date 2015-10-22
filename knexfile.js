9b40b43f1c8dd6bc123d24466659e2ca90a99b03
Arkadiy Kukarkin <arkadiy@mindhole.org>
2015-10-22 17:19:23 -0400

    envs

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,5 @@
 // Update with your config settings.
 var env = require('require-env');
-var URL = require('url');
 
 if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   require('dotenv').load();
@@ -8,20 +7,25 @@ if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 
 var pgServer = env.requireUrl('DATABASE_URL');
 
-module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: env.require('DATABASE_URL')
-  },
-  production: {
-    client: 'postgresql',
-    connection: {
-      host: pgServer.hostname,
-      port: pgServer.port,
-      user: (pgServer.auth || '').split(':')[0],
-      password: (pgServer.auth || '').split(':')[1],
-      database: pgServer.path.substring(1),
-      ssl: true
-    }
+const development = {
+  client: 'postgresql',
+  connection: env.require('DATABASE_URL')
+};
+const production = {
+  client: 'postgresql',
+  connection: {
+    host: pgServer.hostname,
+    port: pgServer.port,
+    user: (pgServer.auth || '').split(':')[0],
+    password: (pgServer.auth || '').split(':')[1],
+    database: pgServer.path.substring(1),
+    ssl: true
   }
+};
+const staging = production;
+
+module.exports = {
+  development,
+  staging,
+  production
 };
